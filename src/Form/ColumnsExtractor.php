48b3422f2951fb998c3d43be8682f5dbb8f18e22
Krzysztof Ruszczynski <ruszczak@o2.pl>
2018-05-15 15:58:39 +0200

    [CustomFieldsColumns] extraction of custom fields implemented #79

--- src/Form/ColumnsExtractor.php
+++ src/Form/ColumnsExtractor.php
@@ -10,6 +10,11 @@ use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 class ColumnsExtractor
 {
     /**
+     * @var int number of columns for custom fields (to know how many empty columns add for records, where custom fields are not present)
+     */
+    protected $customFieldsNumberOfColumns = 0;
+
+    /**
      * Method can be overwritten in order to make custom validation for columns.
      *
      * @param \Symfony\Component\Form\Form $formElement
@@ -55,5 +60,39 @@ class ColumnsExtractor
         }
 
         return $columns;
+    }
+
+    /**
+     * Method adding custom fields labels to given array with column names.
+     * Method counts number of custom fields (result available via getCustomFieldsNumberOfColumns method).
+     *
+     * @param \Symfony\Component\Form\AbstractType $form    form, from which custom fields are extracted
+     * @param array                                $columns current array with column names (can be empty, then return array contains only custom fields)
+     *
+     * @return array array from input with custom fields attached
+     */
+    public function addCustomFieldsColumns($form, $columns = array())
+    {
+        $this->customFieldsNumberOfColumns = 0;
+
+        foreach ($form as $formElement) {
+            $elementOptions = $formElement->getConfig()->getOptions();
+            if (isset($elementOptions['translation_domain']) && $elementOptions['translation_domain'] == 'custom_fields') {
+                $columns[] = $this->getColumnLabel($formElement);
+                $this->customFieldsNumberOfColumns++;
+            }
+        }
+
+        return $columns;
+    }
+
+    /**
+     * Getter for number of custom fields columns (to be called after addCustomFieldsColumns method).
+     *
+     * @return int number of custom fields columns
+     */
+    public function getCustomFieldsNumberOfColumns()
+    {
+        return $this->customFieldsNumberOfColumns;
     }
 }
