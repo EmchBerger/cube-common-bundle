18c2abe62b867d68e2d67ae7a27c8b9785414171
Krzysztof Ruszczynski <ruszczak@o2.pl>
2018-06-11 15:24:50 +0200

    [MailNotificationMechanism] notifications for handling filters extended #73

--- src/Subscriptions/Conditions/Notifications.php
+++ src/Subscriptions/Conditions/Notifications.php
@@ -2,8 +2,11 @@
 
 namespace CubeTools\CubeCommonBundle\Subscriptions\Conditions;
 
+use CubeTools\CubeCommonBundle\Filter\FilterEntityQueryBuilder;
+use CubeTools\CubeCommonBundle\Filter\FilterQueryCondition;
+
 /**
- * Class for checking, if analyzed entity meet requirements to be a subject of subscription.
+ * Class for checking, if analysed entity meet requirements to be a subject of subscription.
  * Method checks, if specified columns where changed. Later filter are checked.
  */
 class Notifications extends AbstractCondition
@@ -28,6 +31,82 @@ class Notifications extends AbstractCondition
      */
     const KEY_FILTER_AFTER = 'filterAfter';
 
+    /**
+     * Key with updated entity
+     */
+    const KEY_ENTITY = 'entity';
+
+    /**
+     * Key with filterform.
+     */
+    const KEY_FILTERFORM = 'form';
+
+    /**
+     * @var \CubeTools\CubeCommonBundle\Filter\FilterEntityQueryBuilder
+     */
+    protected $filterEntityQueryBuilder;
+
+    /**
+     * @var object object having method for creating query builder
+     */
+    protected $filterBuilderObject;
+
+    /**
+     * @var string name of method responsible for creating query builder on $this->filterBuilderObject
+     */
+    protected $filterBuilderMethodName;
+
+    /**
+     * Method setting entity query builder.
+     *
+     * @param \CubeTools\CubeCommonBundle\Filter\FilterEntityQueryBuilder $filterEntityQueryBuilder
+     *
+     * @return $this
+     */
+    public function setFilterEntityQueryBuilder(FilterEntityQueryBuilder $filterEntityQueryBuilder)
+    {
+        $this->filterEntityQueryBuilder = $filterEntityQueryBuilder;
+
+        return $this;
+    }
+
+    /**
+     * Method set object and method name for query builder.
+     *
+     * @param object $filterBuilderObject     object having method for creating query builder
+     * @param string $filterBuilderMethodName name of method responsible for creating query builder (default: createQueryBuilder)
+     *
+     * @return $this
+     */
+    public function setFilterBuilderProvider($filterBuilderObject, $filterBuilderMethodName = 'createQueryBuilder')
+    {
+        $this->filterBuilderObject = $filterBuilderObject;
+        $this->filterBuilderMethodName = $filterBuilderMethodName;
+
+        return $this;
+    }
+
+    /**
+     * Method checks, if entity meets conditions for notification to be made.
+     *
+     * @return bool true if condition is fulfilled
+     */
+    public function isConditionFulfilled()
+    {
+        if ($this->isFilterBeforeSet() || $this->isFilterAfterSet()) {
+            // at least before or after filter is set
+            $conditionFulfilled = $this->executeFilters();
+        } else {
+            $this->setChangedColumns();
+            $conditionFulfilled = !empty($this->outputData[self::KEY_TRIGGER_CHANGED_COLUMNS]);
+        }
+
+        return $conditionFulfilled;
+    }
+
+    /**
+     * Method setting information, which columns which were changed, were subject to trigger.
+     */
     protected function setChangedColumns()
     {
         $changedColumns = array_keys($this->filterData[self::KEY_CHANGESET]);
@@ -42,14 +121,65 @@ class Notifications extends AbstractCondition
     }
 
     /**
-     * Method checks, if entity meets conditions for notification to be made.
+     * Method checks, if filter for entity before change is set.
      *
-     * @return bool true if condition is fulfilled
+     * @return bool true if filter before change set
      */
-    public function isConditionFulfilled()
+    protected function isFilterBeforeSet()
     {
-        $this->setChangedColumns();
+        return (isset($this->filterData[self::KEY_FILTER_BEFORE]) && !is_null($this->filterData[self::KEY_FILTER_BEFORE]));
+    }
 
-        return !empty($this->outputData[self::KEY_TRIGGER_CHANGED_COLUMNS]);
+    /**
+     * Method checks, if filter for entity after change is set.
+     *
+     * @return bool true if filter after change set
+     */
+    protected function isFilterAfterSet()
+    {
+        return (isset($this->filterData[self::KEY_FILTER_AFTER]) && !is_null($this->filterData[self::KEY_FILTER_AFTER]));
+    }
+
+    /**
+     * Method building query on entity.
+     *
+     * @param \CubeTools\CubeCommonBundle\Filter\FilterQueryCondition $filter set Filter
+     * @param \CubeTools\CubeCommonBundle\Form\Type\AbstractFilterType $filterform valid filterform
+     */
+    protected function buildQuery($filter, $filterform)
+    {
+        $this->filterEntityQueryBuilder->resetObject();
+        $this->filterEntityQueryBuilder->setAnalysedEntity($this->filterData[self::KEY_ENTITY]);
+
+        // inserts query builder - every method have a possibility to add it as 3 parameter (default - null)
+        $this->filterBuilderObject->{$this->filterBuilderMethodName}($filter, $filterform, $this->filterEntityQueryBuilder);
+    }
+
+    /**
+     * Method checking analysed entity against filters.
+     *
+     * @return bool true if entity filters matches
+     */
+    protected function executeFilters()
+    {
+        if ($this->isFilterBeforeSet()) {
+            // needs to reproduce state of entity before update
+            // currently always true:
+            $filterBeforeFulfilled = true;
+        } else {
+            $filterBeforeFulfilled = true;
+        }
+
+        if ($this->isFilterAfterSet()) {
+            $filterform = $this->filterData[self::KEY_FILTERFORM];
+            $filter = new FilterQueryCondition($filterform->getData());
+            $filter->setQuerybuilder($this->filterEntityQueryBuilder);
+            $this->buildQuery($filter, $filterform);
+            $filterAfterFulfilled = boolval(count($this->filterEntityQueryBuilder->getQuery()->getResult()));
+        } else {
+            $filterAfterFulfilled = true;
+        }
+
+        return $filterBeforeFulfilled && $filterAfterFulfilled;
     }
 }
