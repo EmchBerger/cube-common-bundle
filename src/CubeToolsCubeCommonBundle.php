0458de2d9434b4d6fc981ffd46bd890e255b99a5
Stefan Paschke <stefan.paschke@gmail.com>
2018-07-09 12:14:48 +0200

    Update CubeToolsCubeCommonBundle.php

--- src/CubeToolsCubeCommonBundle.php
+++ src/CubeToolsCubeCommonBundle.php
@@ -2,12 +2,22 @@
 
 namespace CubeTools\CubeCommonBundle;
 
+use CubeTools\CubeCommonBundle\Security\VersatileFormLoginLdapFactory;
 use CubeTools\CubeCommonDevelop\SymfonyCommands;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\Console\Application;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class CubeToolsCubeCommonBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $extension = $container->getExtension('security');
+        $extension->addSecurityListenerFactory(new VersatileFormLoginLdapFactory());
+    }
+
     public function registerCommands(Application $application)
     {
         parent::registerCommands($application);
